f03d999ece23807cc17cbafb9a6fa19aaad1dcea
Kevin <kevin.qi499@gmail.com>
2025-02-07 01:21:47 -0500

    add user input and functionality for creating grid

--- etch-a-sketch-v2.js
+++ etch-a-sketch-v2.js
@@ -1,2 +1,20 @@
 const gridContainer = document.querySelector('#grid-container')
-const gridSize = prompt("Please enter a desired size (max 100):")
+const gridSize= prompt("Please enter a desired size (max 100):")
+const gridSquareSize = 960 / gridSize;
+
+
+// create the grid given the grid size inputted by user
+function createGrid(){
+    for(let i=0; i <gridSize * gridSize; i++){
+        //create a grid square, append to grid 
+        const gridSquare = document.createElement('div');
+        //assign gridSquare a class for styling
+        gridSquare.classList.add('grid-square')
+        gridSquare.style.width = gridSquareSize + 'px';
+        gridSquare.style.height = gridSquareSize + 'px';
+        //add square to the container
+        gridContainer.appendChild(gridSquare);
+    }
+}
+
+createGrid();
