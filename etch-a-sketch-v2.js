139bdff75ac28e565b6af26b470dd13e7aacbb90
Kevin <kevin.qi499@gmail.com>
2025-02-20 02:39:54 -0500

    add hover effect to grid

--- etch-a-sketch-v2.js
+++ etch-a-sketch-v2.js
@@ -1,11 +1,10 @@
 const gridContainer = document.querySelector('#grid-container')
-const gridSize= prompt("Please enter a desired size (max 100):")
+const gridSize= 10// prompt("Please enter a desired size (max 100):")
 const gridSquareSize = 960 / gridSize;
-
 
 // create the grid given the grid size inputted by user
 function createGrid(){
-    for(let i=0; i <gridSize * gridSize; i++){
+    for(let i=0; i < gridSize * gridSize; i++){
         //create a grid square, append to grid 
         const gridSquare = document.createElement('div');
         //assign gridSquare a class for styling
@@ -13,7 +12,12 @@ function createGrid(){
         gridSquare.style.width = gridSquareSize + 'px';
         gridSquare.style.height = gridSquareSize + 'px';
         //add square to the container
-        gridContainer.appendChild(gridSquare);
+        gridContainer.appendChild(gridSquare)
+
+        //add mouseover event listener to each div
+        gridSquare.addEventListener("mouseover", (event) =>{
+            gridSquare.style.backgroundColor = "black";
+        })
     }
 }
 
